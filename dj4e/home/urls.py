e6d72c58c932f398327de2347b824267f6602939
hanacoon <hanacoon@Hanas-MacBook-Pro-2.local>
2019-02-21 23:09:54 -0500

    yeet

--- dj4e/home/urls.py
+++ dj4e/home/urls.py
@@ -3,5 +3,5 @@ from django.views.generic import TemplateView
 
 # https://docs.djangoproject.com/en/2.1/topics/http/urls/
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='main_home.html'), name='main'),
+    path('', TemplateView.as_view(template_name='main.html'), name='main'),
 ]
